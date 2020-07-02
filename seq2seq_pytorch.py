655873fdc74ad6c624bdd71f755db18e2cf50181
byeongcheoljo <max91128@naver.com>
2020-07-02 10:48:35 +0900

    decoder update
    데이터를 전처리 후 인코더, 디코더, 어텐션 모델을 구현

--- seq2seq_pytorch.py
+++ seq2seq_pytorch.py
@@ -44,7 +44,7 @@ def padSequence(x, max_length):
     else: padded[:len(x)] = x
     return padded
 
-file_name = "file "
+file_name = "data/eng-fra.txt"
 eng_list = []
 fra_list = []
 for ln in open(file_name, 'rt',encoding="utf-8"):
@@ -148,11 +148,10 @@ class Encoder(nn.Module):
         # x transformed = max_len X batch_size X embedding_dim
         # x = x.permute(1,0,2)
         x = pack_padded_sequence(x, lens, batch_first=True) # unpad
-
         self.hidden = self.initialize_hidden_state()
 
         # output: max_length, batch_size, hidden_size
-        # self.hidden: 1, batch_size, hidden_size
+        # self.hidden: 1(one hidden layer), batch_size, hidden_size
         output, self.hidden = self.gru(x, self.hidden) # gru returns hidden state of all timesteps as well as hidden state at last timestep
 
         # pad the sequence to the max length in the batch
@@ -167,8 +166,8 @@ def sort_batch(X, y, lengths):
     lengths, indx = lengths.sort(dim=0, descending=True)
     X = X[indx]
     y = y[indx]
-    return X.transpose(0,1), y, lengths # transpose (batch x seq) to (seq x batch)
-
+    # return X.transpose(0,1), y, lengths # transpose (batch x seq) to (seq x batch)
+    return X,y,lengths
 encoder = Encoder(vocab_inp_size, embedding_dim, hidden_size, batch_size)
 ###test model output
 it = iter(dataset)
@@ -177,7 +176,102 @@ print(x.shape)
 # sort the batch first to be able to use with pac_pack_sequence
 xs, ys, lens = sort_batch(x, y, x_len)
 print(xs, ys, lens)
-enc_output, enc_hidden = encoder(xs, lens)
+enc_output, encoder_hidden = encoder(xs, lens)
 print(enc_output.size()) # max_length, batch_size, hidden_size
-print(enc_hidden.size())
+print(encoder_hidden.size())
 
+
+
+class Decoder(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, dec_units, enc_units, batch_size):
+        super(Decoder, self).__init__()
+        self.batch_size = batch_size
+        self.dec_units = dec_units
+        self.enc_units = enc_units
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.embedding = nn.Embedding(self.vocab_size, self.embedding_dim)
+        self.gru = nn.GRU(self.embedding_dim + self.enc_units,
+                          self.dec_units,
+                          batch_first=True)
+        self.fc = nn.Linear(self.enc_units, self.vocab_size)
+
+        # used for attention
+        self.W1 = nn.Linear(self.enc_units, self.dec_units)
+        self.W2 = nn.Linear(self.enc_units, self.dec_units)
+        self.V = nn.Linear(self.enc_units, 1)
+
+    def forward(self, x, hidden, enc_output):
+        # enc_output converted == (batch_size, max_length, hidden_size)
+        # hidden_with_time_axis shape == (batch_size, 1, hidden size)
+        hidden_with_time_axis = hidden.permute(1, 0, 2)
+
+        # score: (batch_size, max_length, hidden_size)
+        score = torch.tanh(self.W1(enc_output) + self.W2(hidden_with_time_axis))
+
+
+        # attention_weights shape == (batch_size, max_length, 1)
+        # we get 1 at the last axis because we are applying score to self.V
+        attention_weights = torch.softmax(self.V(score), dim=1)
+
+        # context_vector shape after sum == (batch_size, hidden_size)
+        context_vector = attention_weights * enc_output
+        context_vector = torch.sum(context_vector, dim=1)
+
+        # x shape after passing through embedding == (batch_size, 1, embedding_dim)
+        x = self.embedding(x)
+
+        # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
+        x = torch.cat((context_vector.unsqueeze(1), x), -1)
+
+        # output: (batch_size, 1, hidden_size)
+        output, state = self.gru(x)
+
+
+        # output shape == (batch_size * 1, hidden_size)
+        output =  output.view(-1, output.size(2))
+
+        # output shape == (batch_size * 1, vocab)
+        x = self.fc(output)
+
+        return x, state, attention_weights
+
+    def initialize_hidden_state(self):
+        return torch.zeros((1, self.batch_sz, self.dec_units))
+
+
+encoder = Encoder(vocab_inp_size, embedding_dim, hidden_size, batch_size)
+
+it = iter(dataset)
+x, y, x_len = next(it)
+
+print("Input: ", x.shape)
+print("Output: ", y.shape)
+
+xs, ys, lens = sort_batch(x, y, x_len)
+
+enc_output, enc_hidden = encoder(xs, lens)
+print("Encoder Output: ", enc_output.shape) # batch_size X max_length X enc_units
+print("Encoder Hidden: ", enc_hidden.shape) # batch_size X enc_units (corresponds to the last state)
+
+decoder = Decoder(vocab_tar_size, embedding_dim, hidden_size, hidden_size, batch_size)
+
+dec_hidden = enc_hidden
+dec_input = torch.tensor([[target_lang.word2index['<start>']]] * batch_size)
+print("Decoder Input: ", dec_input.shape)
+print("-"*20)
+
+for t in range(1, y.size(1)):
+    # enc_hidden: 1, batch_size, enc_units
+    # output: max_length, batch_size, enc_units
+    predictions, dec_hidden, _ = decoder(dec_input,
+                                         dec_hidden,
+                                         enc_output)
+
+    print("Prediction: ", predictions.shape)
+    print("Decoder Hidden: ", dec_hidden.shape)
+
+
+    dec_input = y[:, t].unsqueeze(1)
+    print(dec_input.shape)
+    break
