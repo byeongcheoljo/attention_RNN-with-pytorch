f88058da0bb75570c4c795dbf1e864f0502e6f27
byeongcheoljo <max91128@naver.com>
2020-07-07 10:41:14 +0900

    테스트 추가

--- attention_LSTM.py
+++ attention_LSTM.py
@@ -26,9 +26,7 @@ for label in labels:
 
 input = torch.LongTensor(input_sentence)
 output = torch.LongTensor(target)
-print(input,"input")
-print(output)
-#
+
 class Model(nn.Module):
     def __init__(self, vocab_size, input_dim, hidden_size, num_classes):
         super().__init__()
@@ -37,41 +35,33 @@ class Model(nn.Module):
         self.fc = nn.Linear(hidden_size, num_classes) # if bidirectional == True, (hidden_size * 2, output)
 
     def attention(self, lstm_output, lstm_hidden_state):
-        print(lstm_output.shape)
-        print(lstm_hidden_state.shape)
-        hidden = lstm_hidden_state.view(-1, hidden_size, 1)
-        print(hidden.shape,"gg")
+        hidden = lstm_hidden_state.view(-1, hidden_size, 1)#[batch_size, hidden_size * num_directions, layer]
         attention_weight = torch.bmm(lstm_output, hidden).squeeze(2) # [batch, sequence_length]
-        print(attention_weight.shape)
         softmax_atten_weight = F.softmax(attention_weight)
-        print(softmax_atten_weight.shape)
          # [batch_size, hidden_size * num_directions(=2), sequence_length] * [batch_size, sequence_length, 1] = [batch_size, hidden_size * num_directions(=2), 1]
         context = torch.bmm(lstm_output.transpose(1,2), softmax_atten_weight.unsqueeze(2)).squeeze(2)
-        print(context.shape)
         # [batch_size, hidden_size * num_bidirectional]
         return context, softmax_atten_weight
 
     def forward(self, x):
         x = self.embedding_layer(x)
-        x = x.view(6, 3, input_dim) # [batch_size, sequence_length, input_dim]
+        x = x.view(-1, 3, input_dim) # [batch_size, sequence_length, input_dim]
         # hidden_0 = torch.zeros(1, 6, hidden_size) # [numlayer * num_bidirectional, batch_size, hidden_size]
         # cell_0 = torch.zeros(1,6,hidden_size) # [num_layer * num_bidirectional, batch_size, hidden_size]
         # out, (hidden, cell) = self.lstm_layer(x, (hidden_0, cell_0))
         out, (hidden, cell) = self.lstm_layer(x)
         attention_output, attention = self.attention(out, hidden)
-
         return self.fc(attention_output), attention
 
 model = Model(vocab_size, input_dim, hidden_size, num_classes)
-print(model(input))
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 # Training
-for epoch in range(5000):
+for epoch in range(50):
     optimizer.zero_grad()
     pred, attention = model(input)
     loss = criterion(pred, output)
-    if (epoch + 1) % 100 == 0:
+    if (epoch + 1) % 10 == 0:
         print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
 
     loss.backward()
@@ -82,8 +72,22 @@ predict, _ = model(input)
 predict = predict.data.max(1, keepdim=True)[1]
 result = (predict.tolist())
 for i in result:
-
     if 1 in i:
         print("Good")
     else:
         print("Bad")
+
+####test
+test_text = 'she loves you'
+tests = [np.asarray([word2index[n] for n in test_text.split()])]
+test_batch = torch.LongTensor(tests)
+test = (test_batch)
+predict, _ = model(test)
+
+result_predict = ((predict.data.max(1, keepdim=True)[1]))
+
+if result_predict[0][0] == 0:
+    print("Bad :(")
+
+else:
+    print("Good :)")
